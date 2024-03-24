201fa2386299fa86a3c5da3f0cff4b5f2cb367f5
ayush amberkar <ayushempire03@gmail.com>
2024-03-24 16:25:47 +0530

    install mongoose and connected to database

--- Backend/server.js
+++ Backend/server.js
@@ -1,23 +1,36 @@
 const express = require("express");
 
+// * importing mongoose
+const mongoose = require("mongoose");
+
 // dot env
 require("dotenv").config();
+
+// * importing router
+const router = require("./routes/router");
 
 // express app
 const app = express();
 
 // middleware
+app.use(express.json());
+
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
 // routes
-app.get("/", (req, res) => {
-  res.json({ msg: "hello world" });
-});
+app.use("/api/workouts/", router);
 
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("listening to port ", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 // listening to port
-app.listen(process.env.PORT, () => {
-  console.log("listening to port ", process.env.PORT);
-});
