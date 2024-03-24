1c46de87a67718fc5e17819c66396a567ba35432
ayush amberkar <ayushempire03@gmail.com>
2024-03-24 16:48:42 +0530

    created the collection workout and post dummy data in to it

--- Backend/routes/router.js
+++ Backend/routes/router.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const router = express.Router();
 
+//* importing model
+const Workout = require("../models/workoutModel");
+
 router.get("/", (req, res) => {
   res.json({ mssg: "get all workouts" });
 });
@@ -10,8 +13,17 @@ router.get("/:id", (req, res) => {
   res.json({ mssg: "get one workout" });
 });
 
-router.post("/", (req, res) => {
-  res.json({ mssg: "POST new Workout" });
+router.post("/", async (req, res) => {
+  const { title, reps, load } = req.body;
+
+  try {
+    const workout = await Workout.create({ title, reps, load });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+
+  // res.json({ mssg: "POST new Workout" });
 });
 
 router.delete("/:id", (req, res) => {
